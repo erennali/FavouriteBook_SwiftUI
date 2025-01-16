ff7e6694b7a7156c6e99c53c617cb1496725d5ff
Eren Ali Koca <eren_ali_koca@hotmail.com>
2025-01-16 15:01:36 +0300

    Initial Commit

---
+++ FavouriteBook/FavouriteBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavouriteBookApp.swift
+//  FavouriteBook
+//
+//  Created by Eren Ali Koca on 16.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct FavouriteBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
